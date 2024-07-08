96cb2a53deb1a241953afc623816a426604017d3
Semion <137element@gmail.com>
2024-07-08 15:40:48 +0800

    modified

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,28 +2,12 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   opts = {
-     flavour = "auto", -- latte, frappe, macchiato, mocha
+    flavour = "frappe", -- latte, frappe, macchiato, mocha
     background = { -- :h background
-        light = "latte",
-        dark = "mocha",
+      light = "latte",
+      dark = "mocha",
     },
- transparent_background = true,
-  color_overrides = {
-    mocha = {
-      text = "#F4CDE9",
-      subtext1 = "#DEBAD4",
-      subtext0 = "#C8A6BE",
-      overlay2 = "#B293A8",
-      overlay1 = "#9C7F92",
-      overlay0 = "#866C7D",
-      surface2 = "#705867",
-      surface1 = "#5A4551",
-      surface0 = "#44313B",
-
-      base = "#352939",
-      mantle = "#211924",
-      crust = "#1a1016",
-    },
-  },
+    transparent_background = false,
+    --
   },
 }
--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -1,32 +1,54 @@
 return {
-  { "akinsho/flutter-tools.nvim", lazy = true }, -- add lsp plugin
+  -- for DAP support
+  { "mfussenegger/nvim-dap" },
   {
-    "AstroNvim/astrolsp",
-    ---@param opts AstroLSPOpts
-    opts = function(plugin, opts)
-      opts.servers = opts.servers or {}
-      table.insert(opts.servers, "dartls")
-
-      opts = require("astrocore").extend_tbl(opts, {
-        setup_handlers = {
-          -- add custom handler
-          dartls = function(_, dartls_opts) require("flutter-tools").setup { lsp = dartls_opts } end,
+    "akinsho/flutter-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
+    config = function()
+      require("flutter-tools").setup {
+        -- (uncomment below line for windows only)
+        -- flutter_path = "home/flutter/bin/flutter.bat",
+        device = "Pixel_3a_API_34_extension_level_7_arm64-v8a",
+        dev_tools = {
+          autostart = true, -- autostart devtools server if not detected
+          auto_open_browser = true, -- Automatically opens devtools in the browser
         },
-        config = {
-          dartls = {
-            -- any changes you want to make to the LSP setup, for example
-            color = {
-              enabled = true,
-            },
-            settings = {
-              renameFilesWithClasses = "always",
-              updateImportsOnRename = true,
-              showTodos = true,
-              completeFunctionCalls = true,
-            },
+        decorations = {
+          statusline = {
+            -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+            -- this will show the current version of the flutter app from the pubspec.yaml file
+            app_version = true,
+            -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+            -- this will show the currently running device if an application was started with a specific
+            -- device
+            device = true,
+            -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+            -- this will show the currently selected project configuration
           },
         },
-      })
+        lsp = {
+          color = { -- show the derived colours for dart variables
+            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            background = true, -- highlight the background
+            background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+            foreground = true, -- highlight the foreground
+            virtual_text = true, -- show the highlight using virtual text
+            virtual_text_str = "■", -- the virtual text character to highlight
+          },
+
+          -- see the link below for details on each option:
+          -- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
+          settings = {
+            showTodos = true,
+            completeFunctionCalls = true,
+
+            renameFilesWithClasses = "prompt", -- "always"
+            enableSnippets = true,
+            updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+          },
+        },
+      }
     end,
   },
+  -- for dart syntax hightling
 }
