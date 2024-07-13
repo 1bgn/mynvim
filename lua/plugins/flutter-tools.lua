8be18b22f9401de3cf8a0a38625c1ebe85e7e819
Semion <137element@gmail.com>
2024-07-14 05:22:41 +0800

    rust correct

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -55,8 +55,8 @@ return {
           -- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
           settings = {
             showTodos = false,
-            completeFunctionCalls = true,
-
+            completeFunctionCalls = false,
+            --
             renameFilesWithClasses = "prompt", -- "always"
             enableSnippets = true,
             updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
